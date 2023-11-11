b2333cdf97275c63054118fa80d3e945361d5682
JavohirDevloper <muhammadiyevj72@gmail.com>
2023-11-12 03:23:30 +0500

    auth for files code

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     CarsModule,
     UsersModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
